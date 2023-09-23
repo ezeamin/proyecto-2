69a2b82f2a66e3f4d2dffccbb33a6baaa1365d2d
Ezequiel Amin <ezequielamin@outlook.com>
2023-09-23 16:22:46 -0300

    creacion de categorias finalizada

---
+++ js/admin/categories/abm.js
@@ -0,0 +1,25 @@
+import { getCategoriesFromLS } from '../../utilities.js';
+import { Category } from './Category.js';
+
+export const createCategory = (name) => {
+  // 1. Obtener categorias de LS
+  const categories = getCategoriesFromLS();
+
+  // 2. Crear categoria (como obj)
+  const newCategory = new Category(name);
+  categories.push(newCategory);
+
+  // 3. Actualizar categorias en LS
+  localStorage.setItem('categories', JSON.stringify(categories));
+
+  // 4. Mensaje de exito
+  swal.fire({
+    title: 'Categoria guardada!',
+    icon: 'success',
+    timer: 1500,
+    timerProgressBar: false,
+    showConfirmButton: false,
+  });
+};
+
+export const editCategory = (name) => {};
---
+++ js/admin/categories/app.js
@@ -0,0 +1,72 @@
+import { getCategoriesFromLS } from '../../utilities.js';
+import { validateName } from '../../validators.js';
+import { createCategory, editCategory } from './abm.js';
+
+// -----------------------------------------
+// 1. Proteger ruta
+// -----------------------------------------
+
+const isLogged = JSON.parse(sessionStorage.getItem('isLogged'));
+if (!isLogged) {
+  // No deberia poder ver la página
+  window.location.href = './login.html';
+}
+
+// -----------------------------------------
+// 2. Seleccionar elementos del DOM
+// -----------------------------------------
+
+const formCategories = document.getElementById('form-categories');
+const categoriesInput = document.getElementById('category-input');
+const credentialsAlert = document.getElementById('alert-categories');
+
+// -----------------------------------------
+// 3. Manejar el submit
+// -----------------------------------------
+
+formCategories.addEventListener('submit', (e) => {
+  // A. Prevenimos comportamiento por defecto
+  e.preventDefault();
+
+  // B. Leer valores de los campos
+  const category = categoriesInput.value;
+
+  // C. Validar los campos (solo el contenido)
+  if (validateName(category, categoriesInput)) {
+    // los campos estan OK pero no sabemos aun si son las credenciales
+
+    // i. Resetear las clases
+    categoriesInput.classList.remove('is-invalid');
+
+    // ii. Corroboramos que no existe otra categoria con el mismo nombre
+    const categories = getCategoriesFromLS();
+    const categoryExists = categories.find(
+      (item) => item.name.toLowerCase() === category.trim().toLowerCase()
+    );
+
+    // La categoria ya existe
+    if (categoryExists) {
+      credentialsAlert.classList.remove('d-none');
+      return;
+    }
+
+    // Si está todo bien -----------------------------------------
+
+    // 1. Ocultar alert
+    credentialsAlert.classList.add('d-none');
+
+    // 2. Crear categoria (como obj)
+    const isEditing = JSON.parse(sessionStorage.getItem('isEditingCategory'));
+    if (isEditing) {
+      editCategory(category);
+    } else {
+      createCategory(category);
+    }
+
+    // 3. Resetear formulario
+    formCategories.reset();
+
+    // 4. Limpiar clases
+    categoriesInput.classList.remove('is-valid');
+  }
+});
